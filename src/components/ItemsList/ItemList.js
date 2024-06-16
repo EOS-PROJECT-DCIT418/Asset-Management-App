32cdc836836fac80e9ae34b3b70345bcdbeb8ca7
rhekord <soanum001@st.ug.edu.gh>
2024-06-16 02:24:27 +0000

    -Added ItemList.js and Added ItemList.styled.js
    Files was used to create the ItemList feature

---
+++ src/components/ItemsList/ItemList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from './ItemList.styled'
+
+const ItemList = () => {
+  return (
+    <Container>
+
+    </Container>
+  )
+}
+
+export default ItemList
