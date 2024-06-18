f154aa6c29c69cd6bb3433e79623abdabedeec36
nadjeley <pinkieaddo@gmail.com>
2024-06-18 18:30:09 +0000

    Worked on the login page with a few things to change like the font and background colour

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import './LoginPage.css';
 const LoginPage = () => {
   return (
-    <div>Login Page</div>
+    <div className='wrapper'>
+      <form action=''>
+       
+        <h1>Welcome back</h1>
+        <h3>Please enter your credentials to sign in</h3>
+        <div className="input-box">
+          <label> Staff ID</label>
+          <input type="text" placeholder='Username' required/>
+        </div>
+        <div className="input-box">
+          <label> Password </label>
+          <input type="password" placeholder='Password' required/>
+        </div>
+
+        <div className="remember-forgot">
+          <label> <input type="checkbox"/>  Remember me</label>
+          <a href="#">Forgot password?</a>
+        </div>
+
+        <button type="submit">Login</button>
+
+        <div className="register-link">
+          <p>Don't have an account? <a href="#">Register</a></p>
+        </div>
+      </form>
+    </div>
+    
   )
 }
 
