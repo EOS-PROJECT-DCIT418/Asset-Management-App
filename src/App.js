f759f1bd8712feb809813b13f75d57dd43df04d2
Alexis Addo <86505575+nadjeley@users.noreply.github.com>
2024-06-18 00:56:56 +0000

    uploaded public and src folders

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+import LoginPage from './pages/LoginPage.js'
+
+function App() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default App;
